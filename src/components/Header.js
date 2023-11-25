263bca27147b31a4495cc7dccbf2745b1bb5e296
edxen <edabogajr@gmail.com>
2023-11-26 02:15:56 +0800

    fix logo not displaying due to gh page reference reqs

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = ({ state }) => {
   return (
     <div className='flex-col-center'>
       <div className="flex-center rounded-tl-md w-full mb-4 bg-black select-none">
-        <img draggable='false' src="/logo.png" width='200px' alt='site-logo'></img>
+        <img draggable='false' src="./logo.png" width='200px' alt='site-logo'></img>
       </div>
       <div className='text-center text-3xl md:text-5xl text-orange-300 p-4'>
         {text.welcome}
